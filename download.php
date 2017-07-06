fa4590d73df1b05629ca5bc2229cfa31bdd5db7b
nikhil kala <nikhilkala8@gmail.com>
2017-07-06 12:14:40 +0530

    Add files via upload

--- download.php
+++ download.php
@@ -22,23 +22,32 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$sql="select filepath,filename from files";
+$sql="select filepath,filename,size from files";
 $result=$conn->query($sql);
 $i=1;
 //echo "$result";
+echo "<table border=0>";
 if ($result->num_rows > 0) 
 {
     // output data of each row
     while($row = $result->fetch_assoc()) 
     {
+    	echo"<tr>";
+    	echo"<td>";
     	echo $i.") "  .'<a href="' . $row['filepath'] .        '" download> '.$row['filename'].'	 </a>';
         $i++;
-        echo "<br>";
+        echo"</td>";
+        echo"<td>";
+        echo $row['size'];
+        echo"</td>";
+        echo"</tr>";
+        //echo "<br>";
     }
 } else 
 {
     echo "No Files To Download";
 }
+echo"</table>";
 $conn->close();
 ?>
 </body>
